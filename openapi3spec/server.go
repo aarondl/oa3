aaa2925874253ec25c1a7ad281fb34b8ed9a8f79
Aaron L <aaron@intensifying.dev>
2022-07-12 11:56:50 -0700

    Fix addressable panic

--- openapi3spec/server.go
+++ openapi3spec/server.go
@@ -9,9 +9,9 @@ import (
 
 // Server represents a server of this API
 type Server struct {
-	URL         string                    `json:"url,omitempty" yaml:"url,omitempty"`
-	Description *string                   `json:"description,omitempty" yaml:"description,omitempty"`
-	Variables   map[string]ServerVariable `json:"variables,omitempty" yaml:"variables,omitempty"`
+	URL         string                     `json:"url,omitempty" yaml:"url,omitempty"`
+	Description *string                    `json:"description,omitempty" yaml:"description,omitempty"`
+	Variables   map[string]*ServerVariable `json:"variables,omitempty" yaml:"variables,omitempty"`
 
 	Extensions `json:"extensions,omitempty" yaml:"extensions,omitempty"`
 }
